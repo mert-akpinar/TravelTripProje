a39e0b111838930763f3e61b1ba202de3fba783b
mert-akpinar <106198602+mert-akpinar@users.noreply.github.com>
2023-05-04 15:39:42 +0300

    Home Slider

--- TravelTripProje/Controllers/DefaultController.cs
+++ TravelTripProje/Controllers/DefaultController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProje.Models.Classes;
 
 namespace TravelTripProje.Controllers
 {
     public class DefaultController : Controller
     {
-        // GET: Default
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var values = c.Blogs.ToList();
+            return View(values);
         }
     }
 }
